dd7c57e8fcea00655e9861dd56cbb50066cfcdd0
Kal'ko Ivan <vano2149@gmail.com>
2022-03-24 14:20:51 +0300

    Initial commit create fish layour application!

---
+++ app/__init__.py
@@ -0,0 +1,9 @@
+"""
+Ядро приложения.
+"""
+
+from flask import Flask
+from app.configs import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
---
+++ manage.py
@@ -0,0 +1,19 @@
+"""
+Входная точка 
+для запуска приложения.
+"""
+
+from app import app
+
+@app.shell_context_processor
+def make_shell_context():
+    """
+    Возвращает словарь для flask shell
+    где ключ - это короткое имя, через которое можно получить 
+    доступ к объекту (value) возвращенного словаря.
+    """
+    return {
+        "app" : app,
+    }
+if __name__ == "__main__":
+    app.run()
