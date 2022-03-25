8a8c65dc73e8b4d1ca1744597adafd98883e4fb1
Kal'ko Ivan <vano2149@gmail.com>
2022-03-25 10:41:34 +0300

    Create data base and install flask_sqlalchemy and flask_migrate package!

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,12 @@
 
 from flask import Flask
 from app.configs import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from app.routes import base
--- app/configs/__init__.py
+++ app/configs/__init__.py
@@ -10,4 +10,6 @@ class Config(object):
     Атрибуты соответствуют полям конфигуратора.
     """
     SECRET_KEY = os.environ.get("SECRET_KEY") or "1234567890"
+    SQLALCHEMY_DATABASE_URL = os.enviroment.get("DATABASE_URI") or "sqlite:///data.db"
+
     
