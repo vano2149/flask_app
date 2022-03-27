aed2aa991a8e77ee611423d313071bdf2a842989
Kal'ko Ivan <vano2149@gmail.com>
2022-03-27 12:51:34 +0300

    Fix bug in an login decorate function!

--- app/models/user.py
+++ app/models/user.py
@@ -6,7 +6,7 @@
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-from app.models.user import login
+from app import login
 
 
 class User(db.Model):
