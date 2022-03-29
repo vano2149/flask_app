c6e6e6fc12bf81ec370fa4ac6cf4c0493e7f424c
Kal'ko Ivan <vano2149@gmail.com>
2022-03-29 15:45:10 +0300

    Fix Bug in an login.html file and Create an user.py file in routes folder!

--- app/forms/user.py
+++ app/forms/user.py
@@ -5,28 +5,28 @@
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length
 from app.models.user import User
 
 class LoginForm(FlaskForm):
     """
     Класс, описывающий форму для логина пользователя.
     """
-    username = StringField(label="Username", validators=[DataRequired()])
+    email = StringField(label="Email", validators=[DataRequired(), Email()])
     password = PasswordField(label="Password", validators=[DataRequired()])
-    remember_me = BooleanField(label="Remembel?")
-    submit = SubmitField(label="log In Button")
+    remember_me = BooleanField(label="Remember Me?")
+    submit = SubmitField(label="log In")
     
 
 class RegisterForm(FlaskForm):
     """
     Класс описывающий Регистрации потльзователя.
     """
-    username = StringField(label="Username", validators=[DataRequired()])
+    username = StringField(label="Username", validators=[DataRequired(), Length(3, 64)])
     email = StringField(label="Email", validators=[DataRequired(),Email()])
     password = PasswordField(label="Password", validators=[DataRequired()])
     password2 = PasswordField(label="Password(again)", validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField(label="Register")
+    submit = SubmitField(label="Sing Up")
 
     def validators_username(self, username):
         user = User.query.filter_by(username=username.data).first()
