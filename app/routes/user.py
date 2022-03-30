4970b47db9993cd5f8e5017eeb337b54875612bb
Kal'ko Ivan <vano2149@gmail.com>
2022-03-30 16:52:58 +0300

    Created flashed messages !

--- app/routes/user.py
+++ app/routes/user.py
@@ -12,10 +12,10 @@ def login():
     form = LoginForm()
     if request.method == "POST" and form.validate():
         if form.email.data == "admin@admin.com" and form.password.data == "admin":
-            flash('You have been logged in!')
+            flash('You have been logged in!', 'success')
             return redirect(url_for("home_page"))
         else:
-            flash('Login Unsuccessfull. Please check your email and password! ')
+            flash('Login Unsuccessfull. Please check your email and password! ', 'danger')
 
     return render_template("login.html", title="login", form=form)
 
@@ -23,6 +23,6 @@ def login():
 def register():
     form = RegisterForm()
     if request.method == "POST" and form.validate():
-        flash(f"Account created for {form.username.data}!")
-        return redirect(url_for('home'))
+        flash(f"Account created for {form.username.data}!", 'success')
+        return redirect(url_for('home_page'))
     return render_template("register.html", title="Register", form=form)
