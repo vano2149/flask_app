7676bc2fb9a183dafd1ca7c1063efca349e5a209
Kal'ko Ivan <vano2149@gmail.com>
2022-03-31 14:45:00 +0300

    Create Posts Form and applying migration!

---
+++ app/forms/post.py
@@ -0,0 +1,16 @@
+"""
+Форма Post.
+"""
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired
+
+class PostForm(FlaskForm):
+    """
+    Клвсс описывающий форму поста пользователя.
+    """
+    title = StringField(label='Title', validators=[DataRequired()])
+    content = TextAreaField(label="Content", validators=[DataRequired()])
+    submit = SubmitField(label="Create Post")
+
--- app/routes/general.py
+++ app/routes/general.py
@@ -4,13 +4,16 @@
 """
 
 from app import app
-from flask import render_template
+from flask import render_template , request
 from flask_login import login_required
-from app.models.post import posts
+from app.models.post import Post, posts # это не этот posts определяется в home_page function.
 
 
-@app.route("/", methods=["GET"])
+@app.route("/")
+@app.route("/home")
 def home_page():
+    page = request.args.get("page", 1, type = int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(page=page, per_page=2)
     return render_template("homepage.html", title="Home Page", posts=posts)
 
 @app.route("/about", methods=["GET"])
