c55744fff58b962f5daedae288c3319d44f4f345
Kal'ko Ivan <vano2149@gmail.com>
2022-03-31 15:46:42 +0300

    Create routes 'user_public' and Modified in an homepage.html file!

--- app/routes/general.py
+++ app/routes/general.py
@@ -7,7 +7,7 @@ from app import app
 from flask import render_template , request
 from flask_login import login_required
 from app.models.post import Post, posts # это не этот posts определяется в home_page function.
-
+from app.models.user import User
 
 @app.route("/")
 @app.route("/home")
@@ -21,3 +21,13 @@ def home_page():
 def about():
     return render_template("about.html", title="About Page")
 
+@app.route("/user/<string:username>")
+@login_required
+def user_public(username:str):
+    """
+    Публичный просмотр постов.
+    """
+    page = request.args.get("page", 1, type=int)
+    user= User.query.filter_by(username=username).first_or_404()
+    posts= Post.query.filter(author=user).order_by(Post.date_posted.desc()).paginate(page=page, per_page=2)
+    return render_template("user_public.html", posts=posts, user=user, title=f"{user.username}'s profile")
