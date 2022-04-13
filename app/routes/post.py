c367fcf7a18a0e70cfd3c87a2f1e6951fdfbfe1f
Kal'ko Ivan <vano2149@gmail.com>
2022-04-13 11:41:04 +0300

    Fix bug in an comment function and create comment_post template!

--- app/routes/post.py
+++ app/routes/post.py
@@ -87,9 +87,9 @@ def comment_post(post_id):
     post = Post.query.get_or_404(post_id)
     form = AddCommetnForm()
     if form.validate_on_submit():
-        comment = Comment(body=form.body.data, post_id=post_id)
+        comment = Comment(body=form.body.data, post_id=post.id)
         db.session.add(comment)
         db.session.commit()
         flash("Your comment has added to the post!", "success")
-        return redirect(url_for("post", post_id=post.id))
+        return redirect(url_for("home_page", post_id=post.id))
     return render_template("comment_post.html", title="Comment Post", form=form)
